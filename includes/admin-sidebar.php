586272c4ce1bb69b3f6a22d0b8490863237e27c7
Laboratorio1pc1 <Laboratorio1pc1@UCATEC.local>
2018-05-30 11:51:07 -0400

    Removed Sucursal ID's from URL

--- includes/admin-sidebar.php
+++ includes/admin-sidebar.php
@@ -3,61 +3,60 @@
     <section class="sidebar">
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu" data-widget="tree">
-            <!-- Optionally, you can add icons to the links -->
             <li class="<?php if($page=='PRINCIPAL'){echo 'active';} ?>">
-                <?php echo "<a href='admin-dashboard.php?ID={$_SESSION['IDsucursal']}'><i class='fa fa-th-large'></i><span>" .'PRINCIPAL'. "</span></a>"; ?>
+                <?php echo "<a href='admin-dashboard.php'><i class='fa fa-th-large'></i><span>" .'PRINCIPAL'. "</span></a>"; ?>
             </li>
             <li class="treeview <?php if($page=='LISTAS'){echo 'active';} ?>">
                 <a href="#"><i class="fa fa-clipboard-list"></i><span>LISTAS</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
                 <ul class="treeview-menu">
                     <li>
-                        <?php echo "<a href='admin-dashboard-proveedor.php?ID={$_SESSION['IDsucursal']}'>" .'Proveedores'. "</a>"; ?>
+                        <?php echo "<a href='admin-dashboard-proveedor.php'>" .'Proveedores'. "</a>"; ?>
                     </li>
                     <li>
-                        <?php echo "<a href='admin-dashboard-marcas.php?ID={$_SESSION['IDsucursal']}'>" .'Marcas'. "</a>"; ?>
+                        <?php echo "<a href='admin-dashboard-marcas.php'>" .'Marcas'. "</a>"; ?>
                     </li>
                     <li>
-                        <?php echo "<a href='admin-dashboard-categoria.php?ID={$_SESSION['IDsucursal']}'>" .'Categorias'. "</a>"; ?>
+                        <?php echo "<a href='admin-dashboard-categoria.php'>" .'Categorias'. "</a>"; ?>
                     </li>
                     <li>
-                        <?php echo "<a href='admin-dashboard-productos.php?ID={$_SESSION['IDsucursal']}'>" .'Productos'. "</a>"; ?>
+                        <?php echo "<a href='admin-dashboard-productos.php'>" .'Productos'. "</a>"; ?>
                     </li>
                 </ul>
             </li>
             <li class="<?php if($page=='INVENTARIO'){echo 'active';} ?>">
-                <?php echo "<a href='admin-dashboard-inventario.php?ID={$_SESSION['IDsucursal']}'><i class='fa fa-archive'></i><span>" .'INVENTARIO'. "</span></a>"; ?>
+                <?php echo "<a href='admin-dashboard-inventario.php'><i class='fa fa-archive'></i><span>" .'INVENTARIO'. "</span></a>"; ?>
             </li>
             <li class="treeview <?php if($page=='TRANSACCION'){echo 'active';} ?>">
                 <a href="#"><i class="fas fa-exchange-alt"></i><span> TRANSACCION</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
                 <ul class="treeview-menu">
                     <li>
-                        <?php echo "<a href='admin-dashboard-compras.php?ID={$_SESSION['IDsucursal']}'>" .'Compras'. "</a>"; ?>
+                        <?php echo "<a href='admin-dashboard-compras.php'>" .'Compras'. "</a>"; ?>
                     </li>
                     <li>
-                        <?php echo "<a href='admin-dashboard-ventas.php?ID={$_SESSION['IDsucursal']}'>" .'Ventas'. "</a>"; ?>
+                        <?php echo "<a href='admin-dashboard-ventas.php'>" .'Ventas'. "</a>"; ?>
                     </li>
                     <li>
-                        <?php echo "<a href='#?ID={$_SESSION['IDsucursal']}'>" .'Devoluciones'. "</a>"; ?>
+                        <?php echo "<a href='#'>" .'Devoluciones'. "</a>"; ?>
                     </li>
                     <li>
-                        <?php echo "<a href='#?ID={$_SESSION['IDsucursal']}'>" .'Intercambios'. "</a>"; ?>
+                        <?php echo "<a href='#'>" .'Intercambios'. "</a>"; ?>
                     </li>
                 </ul>
             </li>
             <li class="<?php if($page=='CUENTASXCOBRAR'){echo 'active';} ?>">
-                <?php echo "<a href='#?ID={$_SESSION['IDsucursal']}'><i class='fa fa-users'></i><span>" .'CUENTAS POR COBRAR'. "</span></a>"; ?>
+                <?php echo "<a href='#']}'><i class='fa fa-users'></i><span>" .'CUENTAS POR COBRAR'. "</span></a>"; ?>
             </li>
             <li class="treeview <?php if($page=='OPCIONES'){echo 'active';} ?>">
                 <a href="#"><i class="fa fa-cogs"></i><span>OPCIONES</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
                 <ul class="treeview-menu">
                     <li>
-                        <?php echo "<a href='#?ID={$_SESSION['IDsucursal']}'>" .'Respaldar base de datos'. "</a>"; ?>
+                        <?php echo "<a href='#'>" .'Respaldar base de datos'. "</a>"; ?>
                     </li>
                     <li>
-                        <?php echo "<a href='#?ID={$_SESSION['IDsucursal']}'>" .'Lista del personal'. "</a>"; ?>
+                        <?php echo "<a href='#'>" .'Lista del personal'. "</a>"; ?>
                     </li>
                     <li>
-                        <?php echo "<a href='#?ID={$_SESSION['IDsucursal']}'>" .'Opciones'. "</a>"; ?>
+                        <?php echo "<a href='#'>" .'Opciones'. "</a>"; ?>
                     </li>
                 </ul>
             </li>
