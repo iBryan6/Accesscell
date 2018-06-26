fc0555be86ff027b3c8463039ca4d43887e6c615
Bryan Argandoña <bryan.argandona@avantica.net>
2018-06-26 11:36:11 -0400

    color dashboard links

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -45,21 +45,22 @@
                     <section class="content container-fluid">
                         <div class="row">
                             <div class="col-md-3 col-sm-6 col-xs-12">
-                                <div class="info-box">
-                                    <span class="info-box-icon bg-yellow"><i class="fas fa-users"></i></span>
-                                    <div class="info-box-content">
-                                        <span class="info-box-text">CLIENTES</span>
-                                        <span class="info-box-number">0</span>
+                                    <div class="info-box">
+                                        <span class="info-box-icon bg-yellow"><i class="fas fa-users"></i></span>
+                                        <div class="info-box-content">
+                                            <span class="info-box-text">CLIENTES</span>
+                                            <span class="info-box-number" style="font-size: 30px;">0</span>
+                                        </div>
+                                        <!-- /.info-box-content -->
                                     </div>
-                                    <!-- /.info-box-content -->
-                                </div>
                             </div>
                             <div class="col-md-3 col-sm-6 col-xs-12">
-                                <div class="info-box">
+                                  <div class="info-box">
+                                   <a href="admin-dashboard-inventario.php">
                                     <span class="info-box-icon bg-aqua"><i class="fas fa-boxes"></i></span>
                                     <div class="info-box-content">
                                         <span class="info-box-text">PRODUCTOS</span>
-                                        <span class="info-box-number">
+                                        <span class="info-box-number" style="font-size: 30px;">
                                         <?php
                                         $sql = "select count(*) as total from producto";
                                         $result = mysqli_query($conn, $sql);
@@ -68,7 +69,7 @@
                                         ?>
                                         </span>
                                     </div>
-                                    <!-- /.info-box-content -->
+                                    </a>
                                 </div>
                             </div>
                             <div class="col-md-3 col-sm-6 col-xs-12">
@@ -76,7 +77,7 @@
                                     <span class="info-box-icon bg-green"><i class="fas fa-shopping-cart"></i></span>
                                     <div class="info-box-content">
                                         <span class="info-box-text">VENTAS</span>
-                                        <span class="info-box-number">
+                                        <span class="info-box-number" style="font-size: 30px;">
                                         <?php
                                         $sql = "select count(*) as total from transaccion where idTipotransaccion=2";
                                         $result = mysqli_query($conn, $sql);
@@ -93,7 +94,7 @@
                                     <span class="info-box-icon bg-black"><i class="fas fa-cart-plus"></i></span>
                                     <div class="info-box-content">
                                         <span class="info-box-text">COMPRAS</span>
-                                        <span class="info-box-number">
+                                        <span class="info-box-number" style="font-size: 30px;">
                                         <?php
                                         $sql = "select count(*) as total from transaccion where idTipotransaccion=1";
                                         $result = mysqli_query($conn, $sql);
@@ -110,7 +111,7 @@
                                     <span class="info-box-icon bg-red"><i class="fas fa-truck"></i></span>
                                     <div class="info-box-content">
                                         <span class="info-box-text">PROVEEDORES</span>
-                                        <span class="info-box-number">
+                                        <span class="info-box-number" style="font-size: 30px;">
                                         <?php
                                         $sql = "select count(*) as total from proveedor";
                                         $result = mysqli_query($conn, $sql);
@@ -127,7 +128,7 @@
                                     <span class="info-box-icon bg-blue"><i class="fas fa-users-cog"></i></span>
                                     <div class="info-box-content">
                                         <span class="info-box-text">PERSONAL</span>
-                                        <span class="info-box-number">
+                                        <span class="info-box-number" style="font-size: 30px;">
                                         <?php
                                         $sql = "select count(*) as total from empleado";
                                         $result = mysqli_query($conn, $sql);
