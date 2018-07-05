d884f4b14920c6bdfad37d795a0b69e0b2148669
Bryan Argandoña <bryan.argandona@avantica.net>
2018-07-05 13:16:44 -0400

    clientes temp fix

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -65,14 +65,14 @@ session_start();
                                         </a>
                                     </div>
                                 </div>
-                                <div class="col-md-3 col-sm-6 col-xs-12">
+                                <!--<div class="col-md-3 col-sm-6 col-xs-12">
                                     <div class="info-box">
                                         <span class="info-box-icon bg-yellow"><i class="fas fa-users"></i></span>
                                         <div class="info-box-content">
                                             <span class="info-box-text">CLIENTES</span>
                                         </div>
                                     </div>
-                                </div>
+                                </div>-->
                                 <div class="col-md-3 col-sm-6 col-xs-12">
                                     <div class="info-box">
                                         <a href="admin-dashboard-inventario.php">
