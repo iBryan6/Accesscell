e20f81c7edc83e7e59b2a1350a8255ebb9f7f420
Bryan Argandoña <bryan.argandona@avantica.net>
2018-10-09 18:17:01 -0400

    google verification

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ require 'includes/connect.php';
     <meta name="keywords" content="Accesscell, Bolivia, Cochabamba, Celulares, Fundas, teléfono, telefono, celular, para celular, carcasa, carcasas para celulares, fundas para celular, donde comprar fundas para celulares">
     <meta name="author" content="Bryan Argandoña">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="IrwzVfBiSzlPj6XnieDPqeZZaZbV1uV_iulS8SvPvgs" />
     <?php include_once('includes/head.php');?>
 </head>
 
